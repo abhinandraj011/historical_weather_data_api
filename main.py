2cdb735d84119b4d698d347de7de2ac8cb94108b
abhinand <abhinandraj0111@gmail.com>
2024-02-29 22:24:26 +0530

    show station table in home page

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pandas
 from flask import Flask, render_template
 #webframe work as mangager for multiple pages in a website and responsible
 import pandas as pd
-import numpy
+
+stations=pd.read_csv("data_small/stations.txt",skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
 
 
 app= Flask(__name__)# we created a website object and now we have to connect html with website object
 @app.route("/")# when home is called the tutorial.html is rendered
 def home():
-    return render_template("home.html") #tutorial.html should be in a specfic folder called templates
+    return render_template("home.html",data=stations.to_html()) #tutorial.html should be in a specfic folder called templates
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
     filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
