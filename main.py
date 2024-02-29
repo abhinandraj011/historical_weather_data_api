17fec8584a002a9e3ea9733308d770c47c8fdfd3
abhinand <abhinandraj0111@gmail.com>
2024-02-29 14:24:57 +0530

    Weather data API developed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import pandas
 from flask import Flask, render_template
 #webframe work as mangager for multiple pages in a website and responsible
+import pandas as pd
+import numpy
+
 
 app= Flask(__name__)# we created a website object and now we have to connect html with website object
 @app.route("/")# when home is called the tutorial.html is rendered
@@ -7,7 +11,9 @@ def home():
     return render_template("home.html") #tutorial.html should be in a specfic folder called templates
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    temperature=23
+    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    df=pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature= df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return{"station": station,
            "date": date,
            "temerature": temperature
